03e495f1056c4e47be28d86b95345c539fc73191
baloghgyorgy <balogh.gyorgy.tfs@gmail.com>
2019-11-15 14:28:11 +0100

    Add project files.

---
+++ FirstXamarinApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FirstXamarinApp.Models;
+
+namespace FirstXamarinApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
